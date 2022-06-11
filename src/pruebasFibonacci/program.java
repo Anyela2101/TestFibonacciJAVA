effe5a8f4e6ad09e7ef5e07c0cdcfd40ff8ea5db
Anyela2101 <anyelasalgado10b2015@gmail.com>
2022-06-11 15:11:56 -0500

    Implementacion prueba

--- src/pruebasFibonacci/program.java
+++ src/pruebasFibonacci/program.java
@@ -2,10 +2,32 @@ package pruebasFibonacci;
 
 public class program {
 
-	public static void main(String[] args) {
+	public static int serieFibonacci(int numero) {
 		
-		
-		
-	}
+		 int num1 = 0;
+         int num2 = 1;
 
+         if (numero == 0)
+         {
+             return 0;
+         }else if (numero == 1 || numero==2)
+         {
+             return 1;
+         }
+         else
+         {
+             for (int i = 0; i < numero; i++)
+             {
+                 int temp = num1;
+                 num1 = num2;
+                 num2 = temp + num2;
+             }
+         }
+
+         
+         return num1;
+     }
+		
 }
+
+
