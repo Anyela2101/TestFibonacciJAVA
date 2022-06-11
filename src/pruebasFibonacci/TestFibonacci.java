b5a4b86900cd3a5295c765646d9b4a73ea8f60cd
Anyela2101 <anyelasalgado10b2015@gmail.com>
2022-06-11 15:16:36 -0500

    Ejecutar pruebas automatizadas

--- src/pruebasFibonacci/TestFibonacci.java
+++ src/pruebasFibonacci/TestFibonacci.java
@@ -12,6 +12,23 @@ class TestFibonacci {
 		assertEquals(0, numero);
 	}
 	
+	@Test
+	void test1() {
+		int numero = program.serieFibonacci(1);
+		assertEquals(1, numero);
+	}
+	
+	@Test
+	void test2() {
+		int numero = program.serieFibonacci(2);
+		assertEquals(1, numero);
+	}
+	
+	@Test
+	void test3() {
+		int numero = program.serieFibonacci(3);
+		assertEquals(1, numero);
+	}
 	
 
 }
